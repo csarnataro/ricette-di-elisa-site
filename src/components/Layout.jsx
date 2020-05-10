a6c559b348165201858cc4e8b3101751fc58c6d8
Christian Sarnataro <christian.sarnataro@vimnmix.com>
2020-05-10 20:09:12 +0200

    Migrated RecipePage to graphql. Added copyright and credits in the footer.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Header from './Header'
+import { GitHub } from 'react-feather'
 
 const Layout = ({ doSearch, children }) => {
   return (
@@ -9,8 +10,23 @@ const Layout = ({ doSearch, children }) => {
       <div className='container mx-auto'>
         <Header doSearch={doSearch} />
         <main className='w-full'>{children}</main>
-        <footer className='border-t border-orange-300 py-10 mt-10'>
-          Copyright here
+        <footer className='text-center border-t border-orange-300 py-10 mt-10 text-gray-600'>
+          <span className='mr-3 inline-block'>
+            &copy; {new Date().getFullYear()} ricette-di-elisa.netlify.app
+          </span>
+          |
+          <span className='mx-3 inline-block'>
+            Design inspired by{' '}
+            <a className='underline' href='https://recipes-demo.marinda.me/'>
+              recipes-demo.marinda.me
+            </a>
+          </span>
+          |
+          <span className='ml-3 inline-block'>
+            <a href='https://github.com/csarnataro/ricette-di-elisa-site'>
+              GitHub <GitHub className='inline-block w-3 h-3' />
+            </a>
+          </span>
         </footer>
       </div>
     </div>
--- src/components/pages/RecipePage.jsx
+++ src/components/pages/RecipePage.jsx
@@ -1,110 +1,70 @@
+import { gql } from 'apollo-boost'
 import dayjs from 'dayjs'
 import 'dayjs/locale/it'
-import maybe from 'mjn'
-
-import React, { useEffect, useState } from 'react'
-import Skeleton from 'react-loading-skeleton'
-import { Link } from 'react-router-dom'
-import { useRouteMatch } from 'react-router-dom'
+import React from 'react'
+import { useQuery } from 'react-apollo'
+import { Link, useRouteMatch } from 'react-router-dom'
 import Layout from '../Layout'
+import Loader from '../Loader'
 import Tag from '../Tag'
-import fetchFromApi from '../../utils/fetch-from-api'
 
-// const fetchRecord = (url, callback) => {
-//   fetch(url)
-//     .then(response => {
-//       if (response.status >= 200 && response.status <= 299) {
-//         return response.json()
-//       }
-//       throw Error(response.statusText)
-//     })
-//     .then(callback)
-//     .catch(error => {
-//       // Handle the error
-//       throw error
-//     })
-// }
-
+const RECIPE_QUERY = gql`
+  query Recipe($id: String!) {
+    recipe(id: $id) {
+      id
+      title
+      tags
+      ingredients
+      process
+    }
+  }
+`
 const RecipePage = () => {
-  const [recipe, setRecipe] = useState({})
+  // const [recipe, setRecipe] = useState({})
   const match = useRouteMatch('/ricetta/:slug')
   const recipeTokens =
     match && match.params.slug && match.params.slug.split('-')
   const recipeId = recipeTokens[recipeTokens.length - 1]
 
-  useEffect(() => {
-    const url = `https://ricette-di-elisa-api.netlify.app/.netlify/functions/recipes/${recipeId}`
-    fetchFromApi(url, record => {
-      if (!record.fields) {
-        return
-      }
-      const categoriesAsString = maybe(record, 'fields.ListaCategorie')
-      const tags = categoriesAsString && categoriesAsString.split(',')
+  const { data, loading, error } = useQuery(RECIPE_QUERY, {
+    variables: { id: recipeId }
+  })
 
-      const retrievedRecipe = {
-        id: record.id,
-        title: maybe(record, 'fields.Name'),
-        ingredients: maybe(record, 'fields.Ingredienti'),
-        process: maybe(record, 'fields.Esecuzione'),
-        suggestedBy: record.fields['Suggerita da'],
-        categoryIds: maybe(record, 'fields.Categoria'),
-        created: record.createdTime,
-        tags
-      }
-      setRecipe(retrievedRecipe)
-    })
-  }, [recipeId])
+  if (loading || !data) return <Loader />
+  if (error) return <div>Error: {JSON.stringify(error)}</div>
 
-  const showSkeleton = !recipe.title
-
+  const { recipe } = data
   return (
     <Layout>
       <div className='mt-2 pl-2'>
-        <Link
-          to='/tutte-le-ricette'
-          className='hover:text-red-700 text-l hover:underline'
-        >
+        <Link to='/' className='hover:text-red-700 text-l hover:underline'>
           &lt; Torna alle ricette
         </Link>
       </div>
       <div>
-        <h2 className='w-full pt-10 text-3xl lg:text-5xl'>
-          {showSkeleton ? <Skeleton width={300} /> : recipe.title}
-        </h2>
+        <h2 className='w-full pt-10 text-3xl lg:text-5xl'>{recipe.title}</h2>
         <div>
           <span className='text-xs text-gray-800 capitalize'>
-            {showSkeleton ? (
-              <Skeleton width={80} />
-            ) : (
-              dayjs(recipe.created)
-                .locale('it')
-                .format('DD MMMM YYYY')
-            )}
+            {dayjs(recipe.created)
+              .locale('it')
+              .format('DD MMMM YYYY')}
           </span>
           <span className='ml-8'>
-            {showSkeleton ? (
-              <Skeleton width={80} />
-            ) : (
-              recipe.tags &&
-              recipe.tags.map(tag => <Tag key={tag}>{`#${tag}`}</Tag>)
-            )}
+            {recipe.tags &&
+              recipe.tags
+                .split(',')
+                .map(tag => <Tag key={tag}>{`#${tag}`}</Tag>)}
           </span>
         </div>
         <h3 className='text-3xl mt-8'>Ingredienti</h3>
-        <div className='text-gray-800'>
-          {showSkeleton ? <Skeleton /> : recipe.ingredients}
-        </div>
+        <div className='text-gray-800'>{recipe.ingredients}</div>
         <h3 className='text-3xl mt-8'>Esecuzione</h3>
         <div className='text-gray-800'>{recipe.process}</div>
-        {showSkeleton ? (
-          <Skeleton lines={2} />
-        ) : (
-          recipe.suggestedBy && (
-            <div className='my-8'>
-              <h3 className='text-3xl'>Suggerita da</h3>
-              <div className='text-gray-800'>{recipe.suggestedBy}</div>
-            </div>
-          )
+        {recipe.suggestedBy && (
+          <div className='my-8'>
+            <h3 className='text-3xl'>Suggerita da</h3>
+            <div className='text-gray-800'>{recipe.suggestedBy}</div>
+          </div>
         )}
       </div>
     </Layout>
